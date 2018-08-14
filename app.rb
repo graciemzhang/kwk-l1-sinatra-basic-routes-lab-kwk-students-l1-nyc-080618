9dff9126c79c0df21c3605bad0d9f23eef1fc9b4
graciemzhang <gracie.zhang@lps-students.org>
2018-08-14 18:58:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "name" do
+    "Gracie Zhang"
+  end
+  
+  get "hometown" do
+    "Sugarland, Texas"
+  end
+  
+  get "favorite-song" do
+    "Location by Khalid"
+  end
 end
